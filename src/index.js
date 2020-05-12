340b9f5c9a50b0d7889a6d6930cdbe621c7073eb
Gabriel Valeriano <gabrielvaleriano18@outlook.com>
2020-05-12 16:23:02 -0300

    Cors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
 mongoose.connect('mongodb+srv://admin:admin@clusterh1-3glzs.gcp.mongodb.net/Chocolate?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 
