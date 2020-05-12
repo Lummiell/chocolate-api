80f2323153375b5ae23a89c21e06ff713cab0ae7
Gabriel Valeriano <gabrielvaleriano18@outlook.com>
2020-05-12 15:16:40 -0300

    Resposta negativa para login

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -4,9 +4,13 @@ const secret = require('../Middleware/secret')
 
 module.exports = {
     async GerarToken(request,response){
+        let auth=false,token=null;
         let {Usuario,Senha} = request.body
         const alunoRetorno = await Aluno.findOne({Login:{Usuario,Senha}})
-        const token = jwt.sign({_id:alunoRetorno._id},secret,{expiresIn:300})
-        return response.send({auth:true,token:token})
+        if(alunoRetorno){
+            auth= true;
+            token = jwt.sign({_id:alunoRetorno._id},secret,{expiresIn:300})
+        }
+        return response.send({auth,token})
     }
 }
